3721f830cb48e238c40ba1569b872f26deb84eca
Hussain Bhatti <hb718107@gmail.com>
2025-10-29 12:40:02 +0500

    inital commit

---
+++ App.js
@@ -0,0 +1,20 @@
+//  let num = 100;
+// String Data Type
+//  const str = 'Hello World';
+// const str1 = 'This is a string created using ${str1} single qoutes';
+// const str2 = "This is a string created using single qoutes";
+// const str3 = `This is a string 
+//             created using ${str1} 
+//             single qoutes`;
+// console.log(str1);
+// console.log(str3);
+// Number Data Type
+// let num = 12;
+// const pi = 3.14;
+// Boolean Data Type
+var isEligible = true;
+var isEqual = false;
+// console.log(Boolean(null));
+// console.log(Boolean('Hello'));
+var isGreater = 10 > 15;
+console.log(isGreater);
---
+++ App.ts
@@ -0,0 +1,24 @@
+//  let num = 100;
+// String Data Type
+//  const str = 'Hello World';
+
+// const str1 = 'This is a string created using ${str1} single qoutes';
+// const str2 = "This is a string created using single qoutes";
+// const str3 = `This is a string 
+//             created using ${str1} 
+//             single qoutes`;
+
+// console.log(str1);
+// console.log(str3);
+// Number Data Type
+// let num = 12;
+// const pi = 3.14;
+// Boolean Data Type
+let isEligible = true;
+let isEqual = false;
+
+// console.log(Boolean(null));
+// console.log(Boolean('Hello'));
+
+let isGreater = 10 > 15;
+console.log(isGreater);
