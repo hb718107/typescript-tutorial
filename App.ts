f7c33a49fa6654837427dfbb7f41d2ffd167694c
Hussain Bhatti <hb718107@gmail.com>
2025-10-30 15:42:20 +0500

    Updated source code

--- App.ts
+++ App.ts
@@ -1,129 +1,152 @@
-// // // // //  let num = 100;
-// // // // // String Data Type
-// // // // //  const str = 'Hello World';
+// // // // // //  let num = 100;
+// // // // // // String Data Type
+// // // // // //  const str = 'Hello World';
 
-// // // // // const str1 = 'This is a string created using ${str1} single qoutes';
-// // // // // const str2 = "This is a string created using single qoutes";
-// // // // // const str3 = `This is a string 
-// // // // //             created using ${str1} 
-// // // // //             single qoutes`;
+// // // // // // const str1 = 'This is a string created using ${str1} single qoutes';
+// // // // // // const str2 = "This is a string created using single qoutes";
+// // // // // // const str3 = `This is a string 
+// // // // // //             created using ${str1} 
+// // // // // //             single qoutes`;
 
-// // // // // console.log(str1);
-// // // // // console.log(str3);
-// // // // // Number Data Type
-// // // // // let num = 12;
-// // // // // const pi = 3.14;
-// // // // // Boolean Data Type
-// // // // let isEligible = true;
-// // // // let isEqual = false;
+// // // // // // console.log(str1);
+// // // // // // console.log(str3);
+// // // // // // Number Data Type
+// // // // // // let num = 12;
+// // // // // // const pi = 3.14;
+// // // // // // Boolean Data Type
+// // // // // let isEligible = true;
+// // // // // let isEqual = false;
 
-// // // // // console.log(Boolean(null));
-// // // // // console.log(Boolean('Hello'));
+// // // // // // console.log(Boolean(null));
+// // // // // // console.log(Boolean('Hello'));
 
-// // // // let isGreater = 10 > 15;
-// // // // console.log(isGreater);
+// // // // // let isGreater = 10 > 15;
+// // // // // console.log(isGreater);
 
-// // // // Type Assignment & Type Interferance
+// // // // // Type Assignment & Type Interferance
 
-// // // function sum(num1 : number,num2 : number,isPrint: boolean){
-// // //     if(isPrint){
-// // //         let s = num1 + num2;
-// // //         console.log("Sum ="+ s);
-// // //     } else{
-// // //         console.log("Sum not found");
-// // //     }
-// // //     return num1 + num2;
+// // // // function sum(num1 : number,num2 : number,isPrint: boolean){
+// // // //     if(isPrint){
+// // // //         let s = num1 + num2;
+// // // //         console.log("Sum ="+ s);
+// // // //     } else{
+// // // //         console.log("Sum not found");
+// // // //     }
+// // // //     return num1 + num2;
+// // // // }
+// // // // let n1: number = 10;
+// // // // let n2: number = 20;
+// // // // // console.log(sum(12,13));
+// // // // console.log(sum(n1,n2,false));
+
+// // // //Object type in typescript
+// // // // let person = {
+// // // //     name: 'John',
+// // // //     age: 30
+// // // // }
+// // // // console.log(person.name);
+// // // // console.log(person["age"]);
+// // // //Arrays In Typescript
+// // // // let person: (string | number)[] = ['john',28,'male',1000];
+// // // // let names: string[]= [];
+// // // // let birthyear: number[] = [1998,1989,2007];
+
+// // // // names.push('john');
+
+// // // // // console.log(names[0]);
+// // // // // console.log(birthyear[0]);
+
+// // // // for(let i of birthyear){
+// // // //     console.log(i);
+// // // // }
+
+// // // //Tuples in typescript
+
+// // // // let employee: [number,string ,number ,boolean] = [123,'john',2000,true,];
+// // // // console.log(employee[1]);
+// // // //////////////Enums in typescript
+// // // // const ADMIN = 0;
+// // // // const READ_ONLY = 1;
+// // // // const WRITE_ONLY = 2;
+// // // // const READ_WRITE = 3;   THIS IS ONE WAY OF DOING WITHOUT ENUMS
+
+// // // enum Roles {
+// // //     ADMIN,
+// // //     READ_ONLY,
+// // //     WRITE_ONLY,
+// // //     READ_WRITE
 // // // }
-// // // let n1: number = 10;
-// // // let n2: number = 20;
-// // // // console.log(sum(12,13));
-// // // console.log(sum(n1,n2,false));
-
-// // //Object type in typescript
-// // // let person = {
+// // // const user = {
 // // //     name: 'John',
-// // //     age: 30
+// // //     age: 30,
+// // //     gender: 'male',
+// // //     role: Roles.ADMIN;
 // // // }
-// // // console.log(person.name);
-// // // console.log(person["age"]);
-// // //Arrays In Typescript
-// // // let person: (string | number)[] = ['john',28,'male',1000];
-// // // let names: string[]= [];
-// // // let birthyear: number[] = [1998,1989,2007];
-
-// // // names.push('john');
-
-// // // // console.log(names[0]);
-// // // // console.log(birthyear[0]);
-
-// // // for(let i of birthyear){
-// // //     console.log(i);
+// // // if(user.role === Roles.ADMIN){
+// // //     console.log('You are an admin user');
 // // // }
+// // // // THIS IS THE WAY OF DOING WITH ENUMS
 
-// // //Tuples in typescript
+// // // //any type in typescript
+// // // let dynamicData: any;
+// // // dynamicData = 100;
 
-// // // let employee: [number,string ,number ,boolean] = [123,'john',2000,true,];
-// // // console.log(employee[1]);
-// // //////////////Enums in typescript
-// // // const ADMIN = 0;
-// // // const READ_ONLY = 1;
-// // // const WRITE_ONLY = 2;
-// // // const READ_WRITE = 3;   THIS IS ONE WAY OF DOING WITHOUT ENUMS
+// // // let arr: any[];
+// // // arr = [100,'hello',true,null,undefined];
 
-// // enum Roles {
-// //     ADMIN,
-// //     READ_ONLY,
-// //     WRITE_ONLY,
-// //     READ_WRITE
+// // // Union Types in Typescript
+// // /*let user: {name: string; age: number} | null = null;
+
+// // function getUser(){
+// //     const uname = 'john';
+// //     const uage = 30;
+// //     user = {name: uname, age: uage};
+// //     return user;
 // // }
-// // const user = {
-// //     name: 'John',
-// //     age: 30,
-// //     gender: 'male',
-// //     role: Roles.ADMIN;
+// // console.log(getUser());*/
+
+// // function printStatus(message: string, statusCode: number | string | boolean){
+// //     if(typeof statusCode === 'string'){
+// //         console.log(`${message} Status : ${statusCode.trim()}`);
+// //     }else{console.log(`${message} Status : ${statusCode}`);}
 // // }
-// // if(user.role === Roles.ADMIN){
-// //     console.log('You are an admin user');
+
+// // printStatus('Success', 200);
+// // printStatus('Error', '500 Internal Server Error');
+// // printStatus('Success',' 200');
+// // printStatus('Success',true);
+// // // printStatus('Not Found', true); // This will give an error
+
+// /////////////////Literal Types in Typescript//////////////
+// const str = 'Hello World' ;
+// let str2 ='Anything' ;
+
+// //Without literal types this function would accept any string value
+
+// // function roleMessage(role: string){
+// //     switch(role){
+// //         case 'admin':
+// //             console.log('You are an admin user');
+// //             break;
+// //         case 'read-only':
+// //             console.log('You are a read-only user');
+// //             break;
+// //         case 'read-write':
+// //             console.log('You are a read-write user');
+// //             break;
+// //             default:
+// //                 console.log('Invalid role');
+// //     }
 // // }
-// // // THIS IS THE WAY OF DOING WITH ENUMS
 
-// // //any type in typescript
-// // let dynamicData: any;
-// // dynamicData = 100;
 
-// // let arr: any[];
-// // arr = [100,'hello',true,null,undefined];
+// // roleMessage('admin');
+// // roleMessage('read-only');
+// // roleMessage('read-write');
+// // roleMessage('super-admin'); // This will give the default case message
 
-// // Union Types in Typescript
-// /*let user: {name: string; age: number} | null = null;
 
-// function getUser(){
-//     const uname = 'john';
-//     const uage = 30;
-//     user = {name: uname, age: uage};
-//     return user;
-// }
-// console.log(getUser());*/
-
-// function printStatus(message: string, statusCode: number | string | boolean){
-//     if(typeof statusCode === 'string'){
-//         console.log(`${message} Status : ${statusCode.trim()}`);
-//     }else{console.log(`${message} Status : ${statusCode}`);}
-// }
-
-// printStatus('Success', 200);
-// printStatus('Error', '500 Internal Server Error');
-// printStatus('Success',' 200');
-// printStatus('Success',true);
-// // printStatus('Not Found', true); // This will give an error
-
-/////////////////Literal Types in Typescript//////////////
-const str = 'Hello World' ;
-let str2 ='Anything' ;
-
-//Without literal types this function would accept any string value
-
-// function roleMessage(role: string){
+// function roleMessage(role: 'admin' | 'read-only' | 'read-write'){
 //     switch(role){
 //         case 'admin':
 //             console.log('You are an admin user');
@@ -143,27 +166,4 @@ let str2 ='Anything' ;
 // roleMessage('admin');
 // roleMessage('read-only');
 // roleMessage('read-write');
-// roleMessage('super-admin'); // This will give the default case message
-
-
-function roleMessage(role: 'admin' | 'read-only' | 'read-write'){
-    switch(role){
-        case 'admin':
-            console.log('You are an admin user');
-            break;
-        case 'read-only':
-            console.log('You are a read-only user');
-            break;
-        case 'read-write':
-            console.log('You are a read-write user');
-            break;
-            default:
-                console.log('Invalid role');
-    }
-}
-
-
-roleMessage('admin');
-roleMessage('read-only');
-roleMessage('read-write');
-// roleMessage('super-admin'); // This will throw an error now
+// // roleMessage('super-admin'); // This will throw an error now
